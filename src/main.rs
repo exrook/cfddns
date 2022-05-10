ddcb2218afab783dab7f2ce19215c59ab29b206b
Jacob Hughes <j@distanthills.org>
2022-05-10 09:14:58 -0400

    Refactor
    Change config format to toml
    Remove email support
    Switch to clap macro
    Use cloudflare to get remote ip
    Remove regex

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::net::Ipv4Addr;
-use std::path::PathBuf;
+use std::path::Path;
 use std::str::FromStr;
 use std::time::Duration;
 
 use anyhow::{anyhow, bail, Result};
-use clap::Parser;
+use clap::{arg, command};
 use cloudflare::endpoints::dns::{DnsContent, DnsRecord};
 use cloudflare::endpoints::{dns, zone};
 use cloudflare::framework::{
@@ -13,32 +13,8 @@ use cloudflare::framework::{
     auth::Credentials,
     Environment, HttpApiClientConfig, OrderDirection,
 };
-use lettre::transport::smtp;
-use lettre::{Message, SmtpTransport, Transport};
 use serde::{Deserialize, Serialize};
-use serde_yaml;
-use tokio::{task, time};
-
-// stolen from https://docs.rs/once_cell/latest/once_cell/index.html
-// MIT @ Aleksey Kladov <aleksey.kladov@gmail.com>
-macro_rules! regex {
-    ($re:literal $(,)?) => {{
-        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
-        RE.get_or_init(|| regex::Regex::new($re).unwrap())
-    }};
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct NotificationConfig {
-    /// Enable email notification
-    enabled: bool,
-
-    /// Sender address
-    from: String,
-
-    /// Recipient address
-    to: String,
-}
+use tokio::time;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
@@ -47,78 +23,34 @@ struct Config {
 
     /// Update interval in seconds (default: 600)
     interval: Option<u64>,
-
-    /// External IP address provider (default: "https://api.ipify.org")
-    endpoint: Option<String>,
-
-    /// Email notification config (default: undefined)
-    notification: Option<NotificationConfig>,
-}
-
-#[derive(Parser, Debug)]
-#[clap(
-    author,
-    version,
-    about,
-    long_about = "Yet another DDNS client for Cloudflare
-[MANUAL] https://github.com/uetchy/cfddns#readme"
-)]
-struct Args {
-    /// Path to domain list file
-    domains: PathBuf,
-
-    /// Path to config file
-    #[clap(short, long)]
-    config: PathBuf,
-}
-
-#[derive(Debug)]
-pub struct MuxWriter {
-    pub buf: Vec<String>,
-    pub should_notify: bool,
-}
-
-impl MuxWriter {
-    pub fn new() -> Self {
-        Self {
-            buf: vec![],
-            should_notify: false,
-        }
-    }
-
-    pub fn mark(&mut self) {
-        self.should_notify = true;
-    }
-
-    pub fn write(&mut self, data: String) {
-        println!("{}", data);
-        self.buf.push(data);
-    }
-
-    pub fn drain(&mut self) -> (String, bool) {
-        let result = self.buf.join("\n").clone();
-        let should_notify = self.should_notify;
-        self.buf.clear();
-        self.should_notify = false;
-        (result, should_notify)
-    }
+    /// TTL in seconds (default: auto)
+    ttl: Option<u32>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // parse args
-    let args = Args::parse();
-    let config_path = args.config;
-    let domain_list_path = args.domains;
+    let matches = command!()
+        .arg(
+            arg!(-c --config <FILE> "Config file")
+                .required(true)
+                .allow_invalid_utf8(true),
+        )
+        .arg(
+            arg!([domains] "File with list of domains to update")
+                .required(true)
+                .allow_invalid_utf8(true),
+        )
+        .arg(arg!(-n --"dry-run" "Dry run, don't actually write changes"))
+        .get_matches();
+    let config_path = matches.value_of_os("config").unwrap();
+    let domain_list_path = matches.value_of_os("domains").unwrap();
+    let dry_run = matches.is_present("dry-run");
 
     // load config
     let config = load_config(&config_path)?;
     let token = config.token.clone();
-    let interval = config.interval.unwrap_or(600);
-    let endpoint = config
-        .endpoint
-        .clone()
-        .unwrap_or_else(|| "https://api.ipify.org".to_string());
+    let interval = config.interval.unwrap_or(0);
 
     // instantiate cloudflare client
     let credentials: Credentials = Credentials::UserAuthToken { token };
@@ -129,164 +61,137 @@ async fn main() -> Result<()> {
     )?;
 
     // preload zone list
-    let mut zone_name_id_map: HashMap<String, String> = HashMap::new();
-    let zones = list_zones(&api_client).await?;
-    for zone in zones {
-        zone_name_id_map.insert(zone.name, zone.id);
+    let mut zone_map = HashMap::new();
+    for (zone_domain, zone_id) in list_zones(&api_client)
+        .await?
+        .iter()
+        .map(|zone| (zone.name.clone(), zone.id.clone()))
+    {
+        let dns_list: HashMap<_, _> = list_dns_records(&zone_id, &api_client)
+            .await?
+            .into_iter()
+            .filter_map(|record| {
+                Some((
+                    match record.content {
+                        DnsContent::A { .. } => record.name.clone(),
+                        _ => return None,
+                    },
+                    record,
+                ))
+            })
+            .collect();
+        zone_map.insert(zone_domain, (zone_id, dns_list));
     }
 
     // load domain list
-    let domain_list = load_domain_list(&domain_list_path)?
-        .iter()
-        .map(|x| {
-            let (fqdn, zone_name) = split_hostname(x).unwrap();
-            (fqdn, zone_name_id_map.get(&zone_name).unwrap().to_owned())
+    let mut domain_list = load_domain_list(&domain_list_path)?
+        .into_iter()
+        .map(|domain| {
+            let (ref zone_id, ref record_map) =
+                zone_map.get(extract_zone(&domain).unwrap()).unwrap();
+            let dns_record = record_map.get(&domain).cloned();
+            (domain, zone_id, dns_record)
         })
-        .collect::<Vec<(String, String)>>();
+        .collect::<Vec<(String, _, _)>>();
 
-    let mut writer = MuxWriter::new();
-
-    let forever = task::spawn(async move {
+    if interval > 0 {
         let mut interval = time::interval(Duration::from_secs(interval));
 
         loop {
             interval.tick().await;
 
-            println!("Started checking DNS records...");
+            println!("Checking DNS records...");
 
-            let (response, should_notify): (String, bool) =
-                match populate_ips(&domain_list, &api_client, &endpoint, &mut writer).await {
-                    Ok(()) => writer.drain(),
-                    Err(err) => (format!("{}", err), true),
-                };
-
-            // notify the result
-            if let Some(nc) = &config.notification {
-                if nc.enabled && should_notify {
-                    println!("Sending an email with config: {:?}", nc);
-                    match send_mail(&nc.from, &nc.to, "cfddns", &response) {
-                        Ok(_) => {}
-                        Err(err) => println!("{}", err),
-                    }
-                }
-            }
+            match populate_ips(&mut domain_list, &api_client, dry_run, config.ttl).await {
+                Err(e) => println!("Error encountered: {:?}", e),
+                _ => (),
+            };
         }
-    });
-
-    forever.await.unwrap();
-
-    Ok(())
+    } else {
+        println!("Checking DNS records...");
+        populate_ips(&mut domain_list, &api_client, dry_run, config.ttl).await
+    }
 }
 
 async fn populate_ips<ApiClientType: ApiClient>(
-    domain_list: &Vec<(String, String)>,
+    // list of (domain, zone_id, dns record)
+    domain_list: &mut [(String, &String, Option<DnsRecord>)],
     api_client: &ApiClientType,
-    endpoint: &str,
-    writer: &mut MuxWriter,
+    dry_run: bool,
+    ttl: Option<u32>,
 ) -> Result<()> {
-    let global_ipv4_addr = get_global_ipv4_addr(&endpoint).await?;
-    writer.write(format!("Current IP address: {}", global_ipv4_addr));
+    let global_ipv4_addr = get_global_ipv4_addr().await?;
 
-    // Build zone id cache
-    let unique_zone_ids: HashSet<String> = domain_list.iter().map(|x| x.1.clone()).collect();
-    let mut fqdn_record_cache: HashMap<String, DnsRecord> = HashMap::new();
-    for zone_id in unique_zone_ids {
-        let records = list_dns_records(&zone_id, api_client).await?;
-        for record in records.into_iter().filter(|x| match x.content {
-            DnsContent::A { content: _ } => true,
-            _ => false,
-        }) {
-            fqdn_record_cache.insert(record.name.clone(), record);
-        }
-    }
-
-    for (fqdn, zone_id) in domain_list {
-        let record = fqdn_record_cache
-            .iter()
-            .find(|x| x.0.eq(fqdn))
-            .and_then(|x| Some(x.1));
-
+    for (fqdn, zone_id, record) in domain_list.iter_mut() {
         if let Some(record) = record {
             let record_ip = match record.content {
                 DnsContent::A { content: ip } => ip,
-                _ => bail!("Invalid content type"),
+                _ => unreachable!("We already filtered out non-A records"),
             };
 
             if record_ip.ne(&global_ipv4_addr) {
-                writer.mark();
-                writer.write(format!(
-                    "Updating A record for {}: {} -> {}",
-                    fqdn, record_ip, global_ipv4_addr
-                ));
-                update_dns_record(
-                    zone_id,
-                    &record.id,
-                    dns::UpdateDnsRecordParams {
-                        ttl: Some(1),
-                        proxied: Some(false),
-                        name: fqdn,
-                        content: dns::DnsContent::A {
-                            content: global_ipv4_addr,
-                        },
-                    },
-                    api_client,
-                )
-                .await?;
-            } else {
-                writer.write(format!("Unchanged {} ({})", fqdn, record_ip));
-            }
-        } else {
-            writer.mark();
-            writer.write(format!(
-                "Creating A record for {} ({})",
-                fqdn, global_ipv4_addr
-            ));
-            create_dns_record(
-                zone_id,
-                dns::CreateDnsRecordParams {
-                    ttl: Some(1),
-                    priority: None,
+                let param = dns::UpdateDnsRecordParams {
+                    ttl,
                     proxied: Some(false),
                     name: fqdn,
                     content: dns::DnsContent::A {
                         content: global_ipv4_addr,
                     },
+                };
+                println!("Updating record for {} to {:?}", fqdn, param);
+                if !dry_run {
+                    update_dns_record(zone_id, &record.id, param, api_client).await?;
+                }
+            } else {
+                println!("Matching record found for {}", fqdn);
+            }
+        } else {
+            let param = dns::CreateDnsRecordParams {
+                ttl,
+                priority: None,
+                proxied: Some(false),
+                name: fqdn,
+                content: dns::DnsContent::A {
+                    content: global_ipv4_addr,
                 },
-                api_client,
-            )
-            .await?;
+            };
+            println!("Creating record for {}: {:?}", fqdn, param);
+            if !dry_run {
+                *record = Some(create_dns_record(zone_id, param, api_client).await?);
+            }
         }
     }
 
     Ok(())
 }
 
-fn load_config(config_path: &PathBuf) -> Result<Config> {
-    let f = std::fs::File::open(config_path)?;
-    let config: Config = serde_yaml::from_reader(f)?;
-    Ok(config)
+fn load_config<P: AsRef<Path>>(config_path: P) -> Result<Config> {
+    let bytes = std::fs::read(config_path)?;
+    Ok(toml::from_slice(&bytes)?)
 }
 
-/// returns `(fqdn, zone)`
-fn split_hostname(name: &str) -> Option<(String, String)> {
-    let re = regex!(r"^(?:[^.]+\.)*?((?:[^.]+\.?){2})$");
-    let cap = re.captures(name).and_then(|cap| {
-        let fqdn = cap.get(0)?.as_str().to_owned();
-        let zone = cap.get(1)?.as_str().to_owned();
-        Some((fqdn, zone))
-    });
-    cap
+/// Extracts example.com from aa.bb.cc.example.com
+fn extract_zone(name: &str) -> Option<&str> {
+    let second_dot = name.rmatch_indices('.').skip(1).next()?.0;
+    Some(name.split_at(second_dot + 1).1)
 }
 
-async fn get_global_ipv4_addr(endpoint: &str) -> Result<Ipv4Addr> {
-    let body = reqwest::get(endpoint).await?.text().await?;
-    match Ipv4Addr::from_str(&body) {
+const CLOUDFLARE_IP_ENDPOINT: &str = "https://1.1.1.1/cdn-cgi/trace";
+
+async fn get_global_ipv4_addr() -> Result<Ipv4Addr> {
+    let body = reqwest::get(CLOUDFLARE_IP_ENDPOINT).await?.text().await?;
+    let ip_str = body
+        .lines()
+        .filter_map(|s| s.strip_prefix("ip="))
+        .next()
+        .ok_or(anyhow!("Malformed cloudflare response"))?;
+    match Ipv4Addr::from_str(&ip_str) {
         Ok(res) => Ok(res),
         Err(err) => bail!(err),
     }
 }
 
-fn load_domain_list(path: &PathBuf) -> Result<Vec<String>> {
+fn load_domain_list<P: AsRef<Path>>(path: P) -> Result<Vec<String>> {
     let data = std::fs::read_to_string(path)?;
     let list = data
         .split("\n")
@@ -365,19 +270,4 @@ async fn update_dns_record<'a, ApiClientType: ApiClient>(
         Ok(res) => Ok(res.result),
         Err(err) => bail!(err),
     }
-}
-
-fn send_mail(from: &str, to: &str, subject: &str, body: &str) -> Result<smtp::response::Response> {
-    let email = Message::builder()
-        .from(from.parse()?)
-        .to(to.parse()?)
-        .subject(subject)
-        .body(body.to_owned())?;
-
-    // Open a local connection on port 25 and send the email
-    let mailer = SmtpTransport::unencrypted_localhost();
-
-    mailer
-        .send(&email)
-        .map_err(|e| anyhow!("Failed to send email: {}", e))
 }
